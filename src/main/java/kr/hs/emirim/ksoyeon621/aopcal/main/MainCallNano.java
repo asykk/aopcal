7e8c44c57177d6f6d6f3f0dd65d9708dbff87b18
Soyeon Kang <ksoyeon621@naver.com>
2018-06-15 10:09:41 +0900

    실행시간 측정하기 3 - 나노시간 측정

---
+++ src/main/java/kr/hs/emirim/ksoyeon621/aopcal/main/MainCallNano.java
@@ -0,0 +1,25 @@
+package kr.hs.emirim.ksoyeon621.aopcal.main;
+
+import kr.hs.emirim.ksoyeon621.aopcal.cal.Calculator;
+import kr.hs.emirim.ksoyeon621.aopcal.cal.ImpeCalculator;
+import kr.hs.emirim.ksoyeon621.aopcal.cal.RecCalculator;
+
+public class MainCallNano {
+
+	public static void main(String[] args) {
+		long num=10;
+		
+		long start1 = System.nanoTime();
+		Calculator cal1 = new ImpeCalculator();
+		long f1 = cal1.factorial(num);
+		long end1 = System.nanoTime();
+		System.out.printf("imp 팩토리얼 %d 실행시간 = %d\n", num, (end1-start1));
+		
+		long start2 = System.nanoTime();
+		Calculator cal2 = new RecCalculator();
+		long f2 = cal2.factorial(num);
+		long end2 = System.nanoTime();
+		System.out.printf("rec 팩토리얼 %d 실행시간 = %d\n", num, (end2-start2));
+	}
+
+}
