9b14ff9a77ecd6ca00f1996904e33f924179c0d3
Soyeon Kang <ksoyeon621@naver.com>
2018-06-19 13:59:39 +0900

    실행시간 측정하기 4 - 프록시 객체로 시간 측정

---
+++ src/main/java/kr/hs/emirim/ksoyeon621/aopcal/main/MainProxy.java
@@ -0,0 +1,19 @@
+package kr.hs.emirim.ksoyeon621.aopcal.main;
+
+import kr.hs.emirim.ksoyeon621.aopcal.cal.ExeTimeCalculator;
+import kr.hs.emirim.ksoyeon621.aopcal.cal.ImpeCalculator;
+import kr.hs.emirim.ksoyeon621.aopcal.cal.RecCalculator;
+
+public class MainProxy {
+
+	public static void main(String[] args) {
+		int num = 20;
+		ExeTimeCalculator ttCal1 = new ExeTimeCalculator(new ImpeCalculator());
+		System.out.println(ttCal1.factorial(num));
+		
+		ExeTimeCalculator ttCal2 =
+				new ExeTimeCalculator(new RecCalculator());
+		System.out.println(ttCal2.factorial(num));
+	}
+
+}
